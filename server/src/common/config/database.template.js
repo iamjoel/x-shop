a709302f629f74f6b1536df19ac728b8fa172e9c
joel <iamjoel007@gmail.com>
2018-04-03 10:21:18 +0800

    remove database

--- server/src/common/config/database.template.js
+++ server/src/common/config/database.template.js
@@ -2,12 +2,12 @@ const mysql = require('think-model-mysql');
 
 module.exports = {
   handle: mysql,
-  database: 'x_shop',
-  prefix: 'x_',
+  database: 'nideshop',
+  prefix: 'nideshop_',
   encoding: 'utf8mb4',
   host: 'localhost',
   port: '3306',
   user: 'root',
-  password: '123456',
+  password: '',
   dateStrings: true
 };
